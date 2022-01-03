11fae3e266f5e220661318d2855058421374678f
tobyrcod <61169763+tobyrcod@users.noreply.github.com>
2022-01-03 10:45:00 +0000

    2: Requests and Responces

--- Learning Resources/Clients and Servers/server.js
+++ Learning Resources/Clients and Servers/server.js
@@ -1,7 +1,23 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-  console.log('request made')
+
+  //Set html content type for response
+  res.setHeader('Content-Type', 'text/html')
+
+  //Send the html file
+  fs.readFile(`${__dirname}/client/index.html`, (err, data) => {
+    if (err) {
+      console.log(err);
+    }
+    else{
+      res.write(data)
+    }
+  });
+  
+  // Send the responce back to the browser
+  res.end()
 });
 
 server.listen(3000, 'localhost', () => {
